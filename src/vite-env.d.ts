1fd4e2f605ba31f6ecd5906ded5e9b86a7060d4b
bernhardbrugger <bernhard.brugger@outlook.com>
2025-06-12 11:21:51 +0200

    Configure reCAPTCHA

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
     readonly VITE_RECAPTCHA_SITE_KEY?: string;
-    // ggf. weitere VITE_-Variablen ...
   }
   interface ImportMeta {
     readonly env: ImportMetaEnv;
   }
-  
